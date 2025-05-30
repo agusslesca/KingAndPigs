483f25ea561065ba456a7b278bc957e22ae455b2
agusslesca <agustinlescanouni@gmail.com>
2025-05-30 16:55:39 -0300

    Flip player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,7 +8,8 @@ public class PlayerController : MonoBehaviour
     private GatherInput m_gatherinput;
     private Transform m_transform;
     [SerializeField] private float speed;
-    
+    private int direction = 1;
+
     void Start()
     {
         m_gatherinput = GetComponent<GatherInput>();
@@ -15,9 +18,25 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    
+
     void FixedUpdate()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        Flip(); //metodo para comprobar si estoy girado o no
         m_rigidbody2D.linearVelocity = new Vector2(speed * m_gatherinput.ValueX, m_rigidbody2D.linearVelocityY);
+
+    }
+
+    private void Flip()
+    {
+        if(m_gatherinput.ValueX * direction < 0)
+        {
+            m_transform.localScale = new Vector3(-m_transform.localScale.x,1,1);
+            direction *= -1;
+        }
     }
 }
